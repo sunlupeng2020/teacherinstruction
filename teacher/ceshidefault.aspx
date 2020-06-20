2892dd6cc9e4d458e1baa1667024e9360ce2841e
sunlupeng2020 <slp2060@163.com>
2020-06-20 23:02:13 +0800

    修改测试管理页面，选择班级，显示该班学生自测次数、平均分等，未完成

--- teacher/ceshidefault.aspx
+++ teacher/ceshidefault.aspx
@@ -5,42 +5,23 @@
     <ContentTemplate>
     <table style="width: 980px">
         <tr>
-            <td style="width: 245px">
+            <td style="width: 245px; text-align: right;">
                 选择班级</td>
             <td style="width: 245px">
-                选择学生</td>
-        </tr>
-        <tr>
-            <td>
-                <asp:ListBox ID="ListBoxbj" runat="server" AutoPostBack="True" 
+                 <asp:DropDownList ID="ddlbj" runat="server" AutoPostBack="True" 
                   DataTextField="banjiname" DataValueField="banjiid" Rows="8" Width="242px" 
-                    onselectedindexchanged="ListBoxbj_SelectedIndexChanged"></asp:ListBox>
-            </td>
-            <td>
-                <asp:ListBox ID="ListBox1" runat="server" 
-                    DataTextField="stu" DataValueField="username" Rows="8" Width="242px"></asp:ListBox>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <br />
-                <asp:LinkButton ID="LinkButtonqtzice" runat="server" 
-                    onclick="LinkButtonqtzice_Click">全体学生自测情况</asp:LinkButton>
-            </td>
-            <td>
-                
-            </td>
-            <td>
-                <asp:LinkButton ID="LinkButton6" runat="server" onclick="LinkButton6_Click">学生自测情况统计</asp:LinkButton>
-                <br />
-            </td>
+                    onselectedindexchanged="ListBoxbj_SelectedIndexChanged" 
+                     AppendDataBoundItems="True">
+                     <asp:ListItem Selected="True" Value="0">--选择班级--</asp:ListItem>
+                 </asp:DropDownList></td>
         </tr>
         <tr>
             <td colspan="2">
-                <asp:Label ID="Label1" runat="server"></asp:Label>
+
+                <asp:GridView ID="GridView1" runat="server">
+                </asp:GridView>
+
             </td>
-            <td>
-                &nbsp;</td>
         </tr>
     </table>
   
--- teacher/ceshidefault.aspx.cs
+++ teacher/ceshidefault.aspx.cs
@@ -23,52 +23,58 @@ public partial class teachermanage_ceshidefault : System.Web.UI.Page
         string username = ((FormsIdentity)HttpContext.Current.User.Identity).Ticket.Name;
         string kechengid=Session["kechengid"].ToString();
         DataSet ds=BanjiInfo.GetTeacherRenkeBanji(username,int.Parse(kechengid));
-        ListBoxbj.DataSource=ds;
-        ListBoxbj.DataBind();
+        ddlbj.DataSource=ds;
+        ddlbj.DataBind();
     }
 
     protected void LinkButton6_Click(object sender, EventArgs e)//学生自测详情
     {
-        if (ListBox1.SelectedIndex >= 0 && ListBoxbj.SelectedIndex >= 0)
-        {
-            string studentusername = ListBox1.SelectedValue;
-            string banjiid = ListBoxbj.SelectedValue;
-            string urlx = "ceshistuzice.aspx?banjiid=" + banjiid + "&studentusername=" + studentusername;
-            string URL = "<script  language= 'javascript'> window.open('" + urlx + "','_blank');</script>";
-            ScriptManager.RegisterClientScriptBlock(this, typeof(string), "stucsxx", URL, false);
-        }
-        else
-        {
-            ScriptManager.RegisterClientScriptBlock(this, typeof(string), "stucsxx", "<script language='javascript'>alert('请选择班级、学生！');</script>", false);
+        //if (ListBox1.SelectedIndex >= 0 && ListBoxbj.SelectedIndex >= 0)
+        //{
+        //    string studentusername = ListBox1.SelectedValue;
+        //    string banjiid = ListBoxbj.SelectedValue;
+        //    string urlx = "ceshistuzice.aspx?banjiid=" + banjiid + "&studentusername=" + studentusername;
+        //    string URL = "<script  language= 'javascript'> window.open('" + urlx + "','_blank');</script>";
+        //    ScriptManager.RegisterClientScriptBlock(this, typeof(string), "stucsxx", URL, false);
+        //}
+        //else
+        //{
+        //    ScriptManager.RegisterClientScriptBlock(this, typeof(string), "stucsxx", "<script language='javascript'>alert('请选择班级、学生！');</script>", false);
 
-        }
+        //}
     }
     protected void LinkButtonqtzice_Click(object sender, EventArgs e)//全体同学自测信息
     {
-        if (ListBoxbj.SelectedIndex >= 0)
-        {
-            string banjiid = ListBoxbj.SelectedValue;
-            string urlx = "ceshiqtzice.aspx?&banjiid=" + banjiid;
-            string URL = "<script  language= 'javascript'> window.open('" + urlx + "','_blank');</script>";
-            ScriptManager.RegisterClientScriptBlock(this, typeof(string), "stucsxx", URL, false);
-        }
-        else
-        {
-            ScriptManager.RegisterClientScriptBlock(this, typeof(string), "stucsxx", "<script language='javascript'>alert('请选择班级！');</script>", false);
-        }
+        //if (ListBoxbj.SelectedIndex >= 0)
+        //{
+        //    string banjiid = ListBoxbj.SelectedValue;
+        //    string urlx = "ceshiqtzice.aspx?&banjiid=" + banjiid;
+        //    string URL = "<script  language= 'javascript'> window.open('" + urlx + "','_blank');</script>";
+        //    ScriptManager.RegisterClientScriptBlock(this, typeof(string), "stucsxx", URL, false);
+        //}
+        //else
+        //{
+        //    ScriptManager.RegisterClientScriptBlock(this, typeof(string), "stucsxx", "<script language='javascript'>alert('请选择班级！');</script>", false);
+        //}
     }
    
-   
+   /// <summary>
+   /// 选择班级后，显示该班学生测试情况
+   /// </summary>
+   /// <param name="sender"></param>
+   /// <param name="e"></param>
     protected void ListBoxbj_SelectedIndexChanged(object sender, EventArgs e)
     {
-        BindBanjiStudent();
+        //查询学生学号、姓名、自测次数、平均成绩，未完待续
+        string sqltxt = "select xuehao,name  count(zice),avg(chengji) from tb_zice where studentid in(select studentid from tb_banjistudent where banjiid='+banjiid+') and kechengid= '" + kechengid + "' group by xuehao order by xuehao";  
+        //BindBanjiStudent();
     }
     protected void BindBanjiStudent()
     {
-        string banjiid = ListBoxbj.SelectedValue;
-        DataTable dt = BanjiInfo.GetStudentNameAndUsername(int.Parse(banjiid));
-        ListBox1.DataSource = dt;
-        ListBox1.DataBind();
+        //string banjiid = ListBoxbj.SelectedValue;
+        //DataTable dt = BanjiInfo.GetStudentNameAndUsername(int.Parse(banjiid));
+        //ListBox1.DataSource = dt;
+        //ListBox1.DataBind();
     }
 
 }
