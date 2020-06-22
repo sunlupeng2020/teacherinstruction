e112407f09d0e280f0867d3427da86aee94b9591
sunlupeng2020 <slp2060@163.com>
2020-06-22 18:18:48 +0800

    在学生自测信息查询页面实现了：选择班级，显示该班学生自测次数、平均分等信息

--- teacher/ceshidefault.aspx
+++ teacher/ceshidefault.aspx
@@ -10,16 +10,62 @@
             <td style="width: 245px">
                  <asp:DropDownList ID="ddlbj" runat="server" AutoPostBack="True" 
                   DataTextField="banjiname" DataValueField="banjiid" Rows="8" Width="242px" 
-                    onselectedindexchanged="ListBoxbj_SelectedIndexChanged" 
                      AppendDataBoundItems="True">
                      <asp:ListItem Selected="True" Value="0">--选择班级--</asp:ListItem>
                  </asp:DropDownList></td>
         </tr>
         <tr>
             <td colspan="2">
-
-                <asp:GridView ID="GridView1" runat="server">
-                </asp:GridView>
+ <asp:GridView ID="GridView2" runat="server" AutoGenerateColumns="False" 
+        DataKeyNames="username" DataSourceID="SqlDataSourcestu" 
+        ondatabound="GridView2_DataBound">
+        <Columns>
+            <asp:TemplateField HeaderText="编号">
+            <ItemTemplate>
+                <asp:Literal ID="Lbh" runat="server"></asp:Literal>
+            </ItemTemplate>
+            </asp:TemplateField>
+            <asp:BoundField DataField="username" HeaderText="学号" ReadOnly="True" 
+                SortExpression="username" />
+            <asp:BoundField DataField="xingming" HeaderText="姓名" 
+                SortExpression="xingming" />
+            <asp:BoundField DataField="xingbie" HeaderText="性别" SortExpression="xingbie" />
+            <asp:TemplateField HeaderText="自测次数">
+                <ItemTemplate>
+                    <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="最高分">
+                <ItemTemplate>
+                    <asp:Literal ID="Literal2" runat="server"></asp:Literal>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="最低分">
+                <ItemTemplate>
+                    <asp:Literal ID="Literal3" runat="server"></asp:Literal>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="平均分">
+                <ItemTemplate>
+                    <asp:Literal ID="Literal4" runat="server"></asp:Literal>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="详情">
+                <ItemTemplate>
+                    <asp:LinkButton ID="LinkButton1" runat="server" 
+                        CommandArgument='<%# Eval("username") %>' onclick="LinkButton1_Click">自测详情</asp:LinkButton>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+    <asp:SqlDataSource ID="SqlDataSourcestu" runat="server" 
+        ConnectionString="<%$ ConnectionStrings:kecheng2012ConnectionString %>" 
+        SelectCommand="SELECT [username],[xingming], [xingbie] FROM [tb_Student] where username in(select studentusername from tb_banjistudent where banjiid=@banjiid) order by [username]">
+        <SelectParameters>
+            <asp:ControlParameter ControlID="ddlbj" Name="banjiid" 
+                PropertyName="SelectedValue" />
+        </SelectParameters>
+    </asp:SqlDataSource>
 
             </td>
         </tr>
--- teacher/ceshidefault.aspx.cs
+++ teacher/ceshidefault.aspx.cs
@@ -63,18 +63,62 @@ public partial class teachermanage_ceshidefault : System.Web.UI.Page
    /// </summary>
    /// <param name="sender"></param>
    /// <param name="e"></param>
-    protected void ListBoxbj_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        //查询学生学号、姓名、自测次数、平均成绩，未完待续
-        string sqltxt = "select xuehao,name  count(zice),avg(chengji) from tb_zice where studentid in(select studentid from tb_banjistudent where banjiid='+banjiid+') and kechengid= '" + kechengid + "' group by xuehao order by xuehao";  
-        //BindBanjiStudent();
-    }
+    //protected void ListBoxbj_SelectedIndexChanged(object sender, EventArgs e)
+    //{
+    //    //查询学生学号、姓名、自测次数、平均成绩，未完待续
+    //    string sqltxt = "select xuehao,name  count(zice),avg(chengji) from tb_zice where studentid in(select studentid from tb_banjistudent where banjiid='+banjiid+') and kechengid= '" + kechengid + "' group by xuehao order by xuehao";  
+    //    //BindBanjiStudent();
+    //}
     protected void BindBanjiStudent()
     {
         //string banjiid = ListBoxbj.SelectedValue;
         //DataTable dt = BanjiInfo.GetStudentNameAndUsername(int.Parse(banjiid));
         //ListBox1.DataSource = dt;
         //ListBox1.DataBind();
+    }
+    protected void GridView2_DataBound(object sender, EventArgs e)
+    {
+        string stuun = "";
+        string kechengid = Session["kechengid"].ToString();
+        SqlConnection conn = new SqlConnection();
+        conn.ConnectionString = ConfigurationManager.ConnectionStrings["kecheng2012ConnectionString"].ConnectionString;
+        SqlCommand comm = conn.CreateCommand();
+        SqlDataReader sdr;
+        conn.Open();
+        int i = 1;
+        foreach (GridViewRow row in GridView2.Rows)
+        {
+            ((Literal)(row.Cells[0].FindControl("Lbh"))).Text = i.ToString();
+            i++;
+            stuun = row.Cells[1].Text.Trim();
+            comm.CommandText = "select count(ziceid),avg(fenshu),max(fenshu),min(fenshu) from tb_zice where kechengid=" + kechengid + " and username='" + stuun + "'";
+            sdr = comm.ExecuteReader();
+            if (sdr.Read())
+            {
+                ((Literal)(row.Cells[4].FindControl("Literal1"))).Text = sdr.GetValue(0).ToString();//自测次数
+                ((Literal)(row.Cells[5].FindControl("Literal2"))).Text = sdr.GetValue(2).ToString();//最高分
+                ((Literal)(row.Cells[6].FindControl("Literal3"))).Text = sdr.GetValue(3).ToString();//最低分
+                if (sdr.GetValue(1).ToString().Trim() != "")
+                {
+                    //((Literal)(row.Cells[7].FindControl("Literal4"))).Text = sdr.GetValue(1).ToString();//平均分
+                    ((Literal)(row.Cells[7].FindControl("Literal4"))).Text = Math.Round(double.Parse(sdr.GetValue(1).ToString())).ToString();//平均分
+                }
+                else
+                {
+                    ((Literal)(row.Cells[7].FindControl("Literal4"))).Text = "0";
+                }
+            }
+            sdr.Close();
+        }
+        conn.Close();
+    }
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        string studentusername = ((LinkButton)sender).CommandArgument;
+        string banjiid = Request.QueryString["banjiid"];
+        string urlx = "ceshistuzice.aspx?banjiid=" + banjiid + "&studentusername=" + studentusername;
+        string URL = "<script  language= 'javascript'> window.open('" + urlx + "','_blank');</script>";
+        ScriptManager.RegisterClientScriptBlock(this, typeof(string), "stucsxx", URL, false);
     }
 
 }
