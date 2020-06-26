df7bce7a43989099170ff2115c464f40af5e1eb8
sunlupeng2020 <slp2060@163.com>
2020-06-26 09:05:27 +0800

    修改测试页面为显示知识点及子孙知识点id

--- timuids.aspx.cs
+++ timuids.aspx.cs
@@ -10,8 +10,6 @@ public partial class timuids : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         ConcreteKnowladge ck1 = new ConcreteKnowladge(85, "ASP.NET概述");
-        List<int> timuid = ck1.GetTimuID();
-        List<int> timuids = ck1.GetTimuIDs();
-        Label1.Text = "a:" + timuid.Count.ToString() + "<br> b:" + timuids.Count.ToString();
+        Label1.Text = "a:" + ck1.ZhishidianIds;
     }
 }
